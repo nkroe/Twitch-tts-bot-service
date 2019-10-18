a5e8b1612e6a38a8af87e658477a4aebe8073227
nkroe <nikitak123@mail.ru>
2019-10-18 16:25:22 +0500

    fix

--- server/lib/clientHandler.js
+++ server/lib/clientHandler.js
@@ -36,12 +36,14 @@ function getClient(client_channel) {
 
     function onMessageHandler(target, context, msg, self) {
 
-        msg = msg.replace(/%/gi, '');
+        if (self) return;
+
+        msg = msg.replace(/%|🇳🇪|π/gi, '');
         let text = msg.split(' ').slice(1).join(' ');
 
-        const emitPlay = () => event.emit('play', {
+        const emitPlay = (t) => event.emit('play', {
             streamer: target.slice(1),
-            text
+            text: t
         });
 
         const updateUsers = u => {
@@ -52,7 +54,7 @@ function getClient(client_channel) {
                 time: Date.now(),
                 type: _type
             });
-            emitPlay();
+            emitPlay(text);
         }
 
         const updateType = (u, message) => {
@@ -75,22 +77,29 @@ function getClient(client_channel) {
 
             if (chan !== target.slice(1)) return;
 
-            if (self) return;
             const isPrem = () => ((context.badges && (context.badges.moderator || context.badges.broadcaster)) || (context.username === 'fake_fake_fake_'));
             const isSub = () => ((type === '2') && (context.badges && (context.badges.subscriber || context.badges.founder || context.badges.vip)));
             const isVip = () => ((type === '3') && (context.badges && context.badges.vip));
+            const isHighlight = () => ((type === '4') && (context['msg-id'] === 'highlighted-message'));
+
             let user = users.find(w => w.name === context.username);
-            if (/^!fake /gi.test(msg)) {
+
+            if (isHighlight()) {
+                const t = text.replace(/пид|ниг|pid|nig/gi, '');
+                emitPlay(t)
+            } else
+
+            if (/^!fake /gi.test(msg) && (type !== '4')) {
                 if (isPrem()) {
                     if (context.username === 'fake_fake_fake') {
-                        emitPlay();
+                        emitPlay(text);
                     } else if (text.length <= 250) {
-                        emitPlay();
+                        emitPlay(text);
                     }
                 } else {
                     if (
                         (muteUsers.map(w => w.name.toLowerCase()).includes(context.username)) ||
-                        (/пид|ниг|pid|nig|🇳🇪|π/.test([...text].filter(w => /([a-zA-Zа-яА-Я0-9])/gi.test(w)).join('')) || text.length > (context.badges && (context.badges.subscriber || context.badges.founder || context.badges.vip) ? 250 : 150)) ||
+                        (/пид|ниг|pid|nig/.test([...text].filter(w => /([a-zA-Zа-яА-Я0-9])/gi.test(w)).join('')) || text.length > (context.badges && (context.badges.subscriber || context.badges.founder || context.badges.vip) ? 250 : 150)) ||
                         (user && ((Date.now() / 1000 - user.time / 1000) < (context.badges && (context.badges.subscriber || context.badges.founder || context.badges.vip) ? 15 : 30)))
                     ) return;
                     if (isSub()) {
@@ -101,34 +110,52 @@ function getClient(client_channel) {
                         updateUsers(user)
                     }
                 }
-            } else if (/^!fakesub$/gi.test(msg) && isPrem()) {
-                updateType('2', 'сабы и выше');
-            } else if (/^!fakevip$/gi.test(msg) && isPrem()) {
-                updateType('3', 'випы и выше');
-            } else if (/^!fakeall$/gi.test(msg) && isPrem()) {
-                updateType('1', 'всех');
-            } else if (/^!skip$/gi.test(msg) && isPrem()) {
-                event.emit('skip', {
-                    streamer: target.slice(1)
-                });
-            } else if (/^!fakemute ([a-zA-Z0-9_])+ ([0-9])+$/gi.test(msg.trim()) && !muteUsers.includes(context.username) && isPrem()) {
-                let user = msg.split(' ')[1].toLowerCase();
-                let time = parseFloat(msg.split(' ')[2]) || 60;
-                event.emit('mute', {
-                    channel: target.slice(1),
-                    name: user,
-                    time: ((Date.now() / 1000) + (time * 60))
-                });
-                console.log(`На канале ${target.slice(1)} был заблокирован пользователь ${user} на ${time} мин.`);
-                client.say(target, `@${context.username} для пользователя @${user} голосовой бот не доступен ${time} мин.`);
-            } else if (/^!fakeunmute ([a-zA-Z0-9_])+$/gi.test(msg.trim()) && !muteUsers.includes(context.username) && isPrem()) {
-                if (msg.split(' ')[1]){
-                    event.emit('unmute', {
-                        channel: target.slice(1),
-                        name: msg.split(' ')[1].toLowerCase()
+            } else
+
+            if (isPrem()) {
+                if (/^!fakesub$/gi.test(msg)) {
+                    updateType('2', 'сабы и выше');
+                } else
+
+                if (/^!fakevip$/gi.test(msg)) {
+                    updateType('3', 'випы и выше');
+                } else
+
+                if (/^!fakemsg$/gi.test(msg)) {
+                    updateType('4', 'выделенные сообщения');
+                } else
+
+                if (/^!fakeall$/gi.test(msg)) {
+                    updateType('1', 'всех');
+                } else
+
+                if (/^!skip$/gi.test(msg)) {
+                    event.emit('skip', {
+                        streamer: target.slice(1)
                     });
-                    console.log(`На канале ${target.slice(1)} был разблокирован пользователь ${msg.split(' ')[1]}`);
-                    client.say(target, `@${context.username} для пользователя @${msg.split(' ')[1]} голосовой бот снова доступен`);
+                } else
+
+                if (/^!fakemute ([a-zA-Z0-9_])+ ([0-9])+$/gi.test(msg.trim()) && !muteUsers.includes(context.username)) {
+                    let user = msg.split(' ')[1].toLowerCase();
+                    let time = parseFloat(msg.split(' ')[2]) || 60;
+                    event.emit('mute', {
+                        channel: target.slice(1),
+                        name: user,
+                        time: ((Date.now() / 1000) + (time * 60))
+                    });
+                    console.log(`На канале ${target.slice(1)} был заблокирован пользователь ${user} на ${time} мин.`);
+                    client.say(target, `@${context.username} для пользователя @${user} голосовой бот не доступен ${time} мин.`);
+                } else
+
+                if (/^!fakeunmute ([a-zA-Z0-9_])+$/gi.test(msg.trim()) && !muteUsers.includes(context.username)) {
+                    if (msg.split(' ')[1]) {
+                        event.emit('unmute', {
+                            channel: target.slice(1),
+                            name: msg.split(' ')[1].toLowerCase()
+                        });
+                        console.log(`На канале ${target.slice(1)} был разблокирован пользователь ${msg.split(' ')[1]}`);
+                        client.say(target, `@${context.username} для пользователя @${msg.split(' ')[1]} голосовой бот снова доступен`);
+                    }
                 }
             }
         })
