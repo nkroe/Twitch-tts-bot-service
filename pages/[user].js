6e9570dcc14c4a367facfa0a6ca57d4476469132
nkroe <nikitak123@mail.ru>
2019-11-07 22:19:20 +0500

    fix

--- pages/[user].js
+++ pages/[user].js
@@ -2,50 +2,73 @@ import io from 'socket.io-client';
 import { useEffect } from 'react';
 import { route } from 'next/dist/next-server/server/router';
 import getConfig from 'next/config';
-import Speech from 'speak-tts'
 
 const { publicRuntimeConfig } = getConfig()
+const voice = ['ru-RU-Wavenet-A', 'ru-RU-Wavenet-B', 'ru-RU-Wavenet-C', 'ru-RU-Wavenet-D'];
+const ttsApi = 'https://api.streamelements.com/kappa/v2/speech';
 
 const User = () => {
 
     useEffect(() => {
-        const speech = new Speech();
-        speech.init({
-            'volume': 2,
-            'lang': 'ru-RU',
-            'rate': 1.7,
-            'pitch': 0.01,
-            'voice': 'Microsoft Irina Desktop - Russian',
-            'splitSentences': true,
-            'listeners': {
-                'onvoiceschanged': () => ''
-            }
-        }).then((data) => '').catch(e => '');
-        
+        let audioCtx = new AudioContext();
+        let audioGain = audioCtx.createGain();
+        audioGain.gain.value = 1;
+        audioGain.connect(audioCtx.destination);
+
         let queue = {
             msg: [],
-            play: false
+            play: false,
+            curr: null,
+            cors: false
         };
-        
+
+        const createUrl = text => {
+            let randVoice = voice[Math.round(0 - 0.5 + Math.random() * (3 - 0 + 1))];
+            if (!queue.cors) {
+                return `${ttsApi}?voice=${randVoice}&text=${text}`;
+            } else {
+                return `https://cors-anywhere.herokuapp.com/${ttsApi}?voice=${randVoice}&text=${text}`;
+            }
+        }
+
         let playQueue = async () => {
             if (queue.play || !queue.msg.length) return;
             queue.play = true;
             loadSound(queue.msg[0]).then(() => playQueue());
         }
-        
+
         let loadSound = text => new Promise(res => {
-            speech.speak({
-                text: text,
-                queue: false,
-                listeners: {
-                    onstart: () => '',
-                    onend: () => {
-                        queue.msg = queue.msg.slice(1);
+            fetch(createUrl(text)).then(data => {
+                if (data.ok) {
+                    data.arrayBuffer().then(data => audioCtx.decodeAudioData(data).then(_data => {
+                        playSound(_data).then(() => {
+                            res();
+                        })
+                    }));
+                } else {
+                    queue.cors = true;
+                    setTimeout(() => {
+                        queue.cors = false;
+                    }, 10000)
+                    setTimeout(() => {
                         queue.play = false;
                         res();
-                    }
+                    }, 2000)
                 }
-            }).then(() => '').catch(e => '')
+            })
+        })
+
+        let playSound = buff => new Promise(res => {
+            let source = audioCtx.createBufferSource();
+            source.buffer = buff;
+            source.connect(audioCtx.destination);
+            source.start();
+            queue.curr = source;
+            source.onended = function () {
+                queue.play = false;
+                queue.msg = queue.msg.slice(1);
+                res();
+            }
         })
 
         const socket = io(publicRuntimeConfig.BACK);
@@ -56,10 +79,12 @@ const User = () => {
                 playQueue();
             }
         });
-        
+
         socket.on(`skip-${window.location.pathname.slice(1)}`, () => {
-            if (queue.msg.length) {
-                speech.cancel();
+            if (queue.curr) {
+                queue.curr.stop();
+                queue.play = false;
+                queue.msg = queue.msg.slice(1);
             }
         });
 
@@ -69,7 +94,7 @@ const User = () => {
 
     }, []);
 
-    return (
+    return ( 
         <>
 
         </>
